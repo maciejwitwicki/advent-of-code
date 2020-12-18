e911602dd1ad4c1f756c222cbcc0de7baac75674
Maciej W <maciej.witwicki@gmail.com>
2020-12-18 08:49:34 +0100

    18

---
+++ src/main/scala/aoc2020/adv18/Advent_18.scala
@@ -0,0 +1,24 @@
+package aoc2020.adv18
+
+import scala.io.Source
+
+object Advent_18 extends App {
+
+  val inputFileName = this.getClass.getSimpleName.replace("_", "-").replace("$", "").toLowerCase
+
+  var inputFile: Array[String] = Source.fromResource(s"aoc2020/$inputFileName.txt").getLines().toArray
+
+  val s1 =
+    """1 + (2 * 3) + (4 * (5 + 6))
+      |2 * 3 + (4 * 5)
+      |5 + (8 * 3 + 9 + 3 * 4 * 3)
+      |5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+      |((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+      |""".stripMargin
+
+   val input1 = Source.fromString(s1).getLines().toArray
+
+   //Part1.solve(inputFile)
+   Part2.solve(inputFile)
+
+}
---
+++ src/main/scala/aoc2020/adv18/Part1.scala
@@ -0,0 +1,82 @@
+package aoc2020.adv18
+
+import scala.collection.mutable
+
+object Part1 {
+
+  def solve(input: Array[String]) = {
+
+    val results = input.map(line => {
+
+      val trimmed = line.replace(" ", "")
+
+      calculate(trimmed)
+
+
+    })
+
+    results.foreach(println)
+    println(s"Result = ${results.sum}")
+
+  }
+
+  private def calculate(line: String): BigDecimal = {
+    var tmpResult = BigDecimal(0)
+    var operation = "S" // S as Starting operation ;)
+    var number = BigDecimal(0)
+
+    def doOperation(n: BigDecimal) = {
+      tmpResult = operation match {
+        case "+" => tmpResult + number
+        case "*" => tmpResult * number
+        case "S" => operation = "X"; number // allow first unknown operation, next time throw exception
+        case _   => throw new RuntimeException("illegal operation")
+      }
+    }
+
+    var it = 0
+    while(it < line.length) {
+
+      val element = line(it)
+
+      element match {
+        case '(' => {
+          val expression = extract(line.substring(it))
+          number = calculate(expression)
+          doOperation(number)
+          it = it + expression.length + 2 // count opening and closing parentheses in
+        }
+        case '+' => operation = "+"; it = it + 1
+        case '*' => operation = "*"; it = it + 1
+        case x   => {
+          number = BigDecimal(x.toString)
+          doOperation(number)
+          it = it + 1
+        }
+      }
+    }
+
+    tmpResult
+  }
+
+  private def extract(text: String) = {
+
+    var it = 1
+    var openParentheses = 1
+    while(openParentheses > 0) {
+      val char = text(it)
+
+      char match {
+        case '(' => openParentheses = openParentheses + 1
+        case ')' => openParentheses = openParentheses - 1
+        case _ =>
+      }
+
+      it = it + 1
+    }
+
+    text.drop(1).take(it - 2)
+  }
+}
+
+
