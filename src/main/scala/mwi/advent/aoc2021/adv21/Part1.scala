55ec05cccf48cb3d7d58c37a11c734908e23872a
PC\Maciek <maciej.witwicki@gmail.com>
2021-12-21 07:44:13 +0100

    2021 - 21

--- src/main/scala/mwi/advent/aoc2021/adv21/Part1.scala
+++ src/main/scala/mwi/advent/aoc2021/adv21/Part1.scala
@@ -12,6 +12,82 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 object Part1 extends NumberExtractor {
   private implicit val ec: ExecutionContext = ExecutionContext.global
 
-  def solve(input: Array[String]): Unit = {}
+  var die = 0
+
+  def solve(input: Array[String]): Unit = {
+
+    var p1 = extractNumbers(input(0)).toList.last.toInt
+    var p2 = extractNumbers(input(1)).toList.last.toInt
+
+    var p1Score = 0
+    var p2Score = 0
+
+    var turns = 0
+
+    while (p1Score < 1000 && p2Score < 1000) {
+
+      val thrown1 = nextDices()
+      val nextP1  = p1 + thrown1.sum
+
+      if (nextP1 > 10) {
+        p1 = nextP1 % 10
+        if (p1 == 0) p1 = 10
+      } else {
+        p1 = nextP1
+      }
+      p1Score += p1
+
+      println(s"Player 1 rolls for ${thrown1.mkString("+")} and moves to $p1 for total $p1Score")
+
+      if (p1Score < 1000) {
+
+        val thrown2 = nextDices()
+        val nextP2  = p2 + thrown2.sum
+
+        if (nextP2 > 10) {
+          p2 = nextP2 % 10
+          if (p2 == 0) p2 = 10
+        } else {
+          p2 = nextP1
+        }
+        p2Score += p2
+
+        println(s"Player 2 rolls for ${thrown2.mkString("+")} and moves to $p2 for total $p2Score")
+
+      }
+
+      turns += 1
+    }
+
+    val dieThrows = if (p1Score >= 1000) {
+      turns * 6 - 3
+    } else {
+      turns * 6
+    }
+
+    println(s"$turns: p1: $p1Score, p2: $p2Score")
+    val min = Math.min(p1Score, p2Score)
+    val res = dieThrows * min
+
+    println(s"res: $res")
+
+  }
+
+  private def nextDices() = {
+
+    var rolls = ArrayBuffer.empty[Int]
+
+    for (i <- 0 until 3) {
+
+      die += 1
+
+      if (die > 100) die = 1
+
+      rolls.append(die)
+    }
+
+    rolls
+
+  }
 
 }
---
+++ src/main/scala/mwi/advent/aoc2021/adv21/Part2.scala
@@ -0,0 +1,60 @@
+package mwi.advent.aoc2021.adv21
+
+import mwi.advent.aoc2021.adv19.impl.Parser
+import mwi.advent.util.{Loc, NumberExtractor}
+
+import java.math.BigInteger
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+object Part2 extends NumberExtractor {
+  private implicit val ec: ExecutionContext = ExecutionContext.global
+
+  var die = 0
+
+  private var cache = mutable.HashMap.empty[(Int, Int, Int, Int), (Long, Long)]
+
+  def solve(input: Array[String]): Unit = {
+
+    val p1 = extractNumbers(input(0)).toList.last.toInt
+    val p2 = extractNumbers(input(1)).toList.last.toInt
+
+    val res = countWin(p1 - 1, p2 - 1, 0, 0)
+
+    println(res)
+
+  }
+
+  private def countWin(p1: Int, p2: Int, s1: Int, s2: Int): (Long, Long) = {
+
+    // return state if someone wins
+    if (s1 >= 21) {
+      (1L, 0L)
+    } else if (s2 >= 21) {
+      (0L, 1L)
+    } else {
+      cache.getOrElse(
+        (p1, p2, s1, s2), {
+
+          var res = (0L, 0L)
+
+          for (d1 <- 1 to 3) {
+            for (d2 <- 1 to 3) {
+              for (d3 <- 1 to 3) {
+                val newP1 = (p1 + d1 + d2 + d3) % 10
+                val newS1 = s1 + newP1 + 1
+
+                val (x1, y1) = countWin(p2, newP1, s2, newS1)
+                res = (res._1 + y1, res._2 + x1)
+              }
+            }
+          }
+          cache.put((p1, p2, s1, s2), res)
+          res
+        }
+      )
+    }
+  }
+}
